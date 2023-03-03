59e5a79ed13380d7752b3dee0f7b3f369a5eef94
dylancdavis <dylcdav@gmail.com>
2023-03-03 15:17:13 -0500

    Add quiz summary functionality

--- src/components/QuizSummary.vue
+++ src/components/QuizSummary.vue
@@ -1,7 +1,16 @@
 <template>
 	<div id="quiz-summary">
 		<h1>Quiz Summary</h1>
-			<div></div>
+		<div v-for="(question,qIndex) in questions" :key="qIndex">
+			<div>Q{{qIndex+1}}: {{question.text}}</div>
+			<ul>
+				<li
+					v-for="(answer, aIndex) in question.answers" 
+					:key="aIndex"
+					:class="{chosen: answers[qIndex] === aIndex}"
+				>{{ answer }}</li>
+			</ul>
+		</div>
 	</div>
 </template>
 
@@ -10,3 +19,21 @@ export default {
 	props: ["questions","answers"]
 }
 </script>
+
+<style scoped>
+
+#quiz-summary {
+	border: 2px solid black;
+	padding: 16px;
+	border-radius: 16px;
+}
+
+h1 {
+	margin-top: 0px;
+}
+
+.chosen {
+	background-color: rgba(0, 195, 255, 0.221);
+}
+
+</style>
